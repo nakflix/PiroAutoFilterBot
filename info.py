d4d2a38a28662ac6779815c4c1a76c0258859fc2
nakflix <123237114+nakflix@users.noreply.github.com>
2025-01-13 11:41:53 +0300

    Update info.py

--- info.py
+++ info.py
@@ -51,8 +51,8 @@ SUPPORT_CHAT_ID = -1001585995212
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "PIRO")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
 # Others
